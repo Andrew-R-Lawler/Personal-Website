ae32ac078904e9b2e4eaa3327c43c646742ea8ba
Andrew-R-Lawler <61293731+Andrew-R-Lawler@users.noreply.github.com>
2020-08-17 17:04:12 -0500

    express server runs

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
+
 const express = require('express');
-const bodyParser = require('body-parser');
+require('dotenv').config();
+
 const app = express();
+const bodyParser = require('body-parser');
+
 
 // Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve static files
-app.use(express.static('public'));
+app.use(express.static('build'));
 
 // App Set //
 const PORT = process.env.PORT || 5000;
